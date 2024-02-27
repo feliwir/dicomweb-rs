e36a40804038636624c566fc703734838b107dd5
Stephan Vedder <vedder@mbits.info>
2024-02-27 14:16:07 +0100

    Reject requests without "Accept" header

--- server/src/actix/qido.rs
+++ server/src/actix/qido.rs
@@ -1,14 +1,23 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, http, web, HttpRequest, HttpResponse, Responder};
 use dicom_json::DicomJson;
 use dicom_object::InMemDicomObject;
+
 
 use crate::{DicomWebServer, QidoInstanceQuery, QidoSeriesQuery, QidoStudyQuery};
 
 #[get("/studies")]
 pub async fn search_studies_all(
+    request: HttpRequest,
     callbacks: web::Data<DicomWebServer>,
     query: web::Query<QidoStudyQuery>,
 ) -> impl Responder {
+    // See https://dicom.nema.org/medical/dicom/current/output/html/part18.html#sect_8.7.5
+    // "The response to a request without an Accept header field shall be 406 (Not Acceptable)"
+    let accept_header = request.headers().get(http::header::ACCEPT);
+    if accept_header.is_none() {
+        return HttpResponse::NotAcceptable().finish();
+    }
+
     let result = (callbacks.search_study)(&query);
 
     match result {
@@ -31,10 +40,18 @@ pub async fn search_studies_all(
 
 #[get("/studies/{study_uid}/series")]
 pub async fn search_series_study_level(
+    request: HttpRequest,
     callbacks: web::Data<DicomWebServer>,
     study_uid: web::Path<String>,
     query: web::Query<QidoSeriesQuery>,
 ) -> impl Responder {
+    // See https://dicom.nema.org/medical/dicom/current/output/html/part18.html#sect_8.7.5
+    // "The response to a request without an Accept header field shall be 406 (Not Acceptable)"
+    let accept_header = request.headers().get(http::header::ACCEPT);
+    if accept_header.is_none() {
+        return HttpResponse::NotAcceptable().finish();
+    }
+
     let result = (callbacks.search_series)(Some(&study_uid), &query);
 
     match result {
@@ -57,10 +74,18 @@ pub async fn search_series_study_level(
 
 #[get("/studies/{study_uid}/instances")]
 pub async fn search_instances_study_level(
+    request: HttpRequest,
     callbacks: web::Data<DicomWebServer>,
     study_uid: web::Path<String>,
     query: web::Query<QidoInstanceQuery>,
 ) -> impl Responder {
+    // See https://dicom.nema.org/medical/dicom/current/output/html/part18.html#sect_8.7.5
+    // "The response to a request without an Accept header field shall be 406 (Not Acceptable)"
+    let accept_header = request.headers().get(http::header::ACCEPT);
+    if accept_header.is_none() {
+        return HttpResponse::NotAcceptable().finish();
+    }
+
     let result = (callbacks.search_instances)(Some(&study_uid), None, &query);
 
     match result {
@@ -83,9 +108,17 @@ pub async fn search_instances_study_level(
 
 #[get("/series")]
 pub async fn search_series_all(
+    request: HttpRequest,
     callbacks: web::Data<DicomWebServer>,
     query: web::Query<QidoSeriesQuery>,
 ) -> impl Responder {
+    // See https://dicom.nema.org/medical/dicom/current/output/html/part18.html#sect_8.7.5
+    // "The response to a request without an Accept header field shall be 406 (Not Acceptable)"
+    let accept_header = request.headers().get(http::header::ACCEPT);
+    if accept_header.is_none() {
+        return HttpResponse::NotAcceptable().finish();
+    }
+
     let result = (callbacks.search_series)(None, &query);
 
     match result {
@@ -108,11 +141,19 @@ pub async fn search_series_all(
 
 #[get("/studies/{study_uid}/series/{series_uid}/instances")]
 pub async fn search_instances_series_level(
+    request: HttpRequest,
     callbacks: web::Data<DicomWebServer>,
     study_uid: web::Path<String>,
     series_uid: web::Path<String>,
     query: web::Query<QidoInstanceQuery>,
 ) -> impl Responder {
+    // See https://dicom.nema.org/medical/dicom/current/output/html/part18.html#sect_8.7.5
+    // "The response to a request without an Accept header field shall be 406 (Not Acceptable)"
+    let accept_header = request.headers().get(http::header::ACCEPT);
+    if accept_header.is_none() {
+        return HttpResponse::NotAcceptable().finish();
+    }
+
     let result = (callbacks.search_instances)(Some(&study_uid), Some(&series_uid), &query);
 
     match result {
@@ -135,9 +176,17 @@ pub async fn search_instances_series_level(
 
 #[get("/instances")]
 pub async fn search_instances_all(
+    request: HttpRequest,
     callbacks: web::Data<DicomWebServer>,
     query: web::Query<QidoInstanceQuery>,
 ) -> impl Responder {
+    // See https://dicom.nema.org/medical/dicom/current/output/html/part18.html#sect_8.7.5
+    // "The response to a request without an Accept header field shall be 406 (Not Acceptable)"
+    let accept_header = request.headers().get(http::header::ACCEPT);
+    if accept_header.is_none() {
+        return HttpResponse::NotAcceptable().finish();
+    }
+
     let result = (callbacks.search_instances)(None, None, &query);
 
     match result {
